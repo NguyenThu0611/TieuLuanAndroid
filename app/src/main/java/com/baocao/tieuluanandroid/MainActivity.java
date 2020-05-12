0b661d700a478d8f65f1a03ab40982792bec3a0b
vietsaclo <vietsaclo@gmail.com>
2020-05-12 17:47:04 +0700

    Spinner 1

--- app/src/main/java/com/baocao/tieuluanandroid/MainActivity.java
+++ app/src/main/java/com/baocao/tieuluanandroid/MainActivity.java
@@ -24,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        Spinner spinner = findViewById(R.id.spinner);
 
+        //Tao Adapter cho spinner
+        List<String> list = new ArrayList<>();
+        list.add("Item 1");
+        list.add("Item 2");
+        list.add("Item 3");
+        list.add("Item 4");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spinner.setAdapter(adapter);
     }
 }
