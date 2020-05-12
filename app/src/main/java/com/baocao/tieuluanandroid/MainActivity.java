488cabac3860e655e9dbbf36c21d78ee3a67fa7e
vietsaclo <vietsaclo@gmail.com>
2020-05-12 15:13:03 +0700

    Demo Spinner

--- app/src/main/java/com/baocao/tieuluanandroid/MainActivity.java
+++ app/src/main/java/com/baocao/tieuluanandroid/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void init(){
         btnClick = findViewById(R.id.btnClick);
+        spinner = findViewById(R.id.spinner);
+
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,7 +33,20 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Ok Vu Truong", Toast.LENGTH_SHORT).show();
             }
         });
+
+        //nap du lieu cho spinner;
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spinner.setAdapter(adapter);
     }
 
+    //control
     private Button btnClick;
+    private Spinner spinner;
 }
