ed4233f82d9abab127fe41b9a54551414e0848f4
NguyenThu0611 <nthu061198@gmail.com>
2020-05-16 15:12:04 +0700

    cap nhat GridView

--- app/src/main/java/com/baocao/tieuluanandroid/GridViewActivity.java
+++ app/src/main/java/com/baocao/tieuluanandroid/GridViewActivity.java
@@ -2,10 +2,12 @@ package com.baocao.tieuluanandroid;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ListAdapter;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,10 +28,17 @@ public class GridViewActivity extends AppCompatActivity {
     private void init(){
         gridView = findViewById(R.id.gridView);
 
-        List<String> list = new ArrayList<String>(Arrays.asList("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",")));
+        final List<String> list = new ArrayList<String>(Arrays.asList("0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",")));
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
         gridView.setAdapter(arrayAdapter);
         gridView.setNumColumns(10);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(GridViewActivity.this, "Item: "+list.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private GridView gridView;
